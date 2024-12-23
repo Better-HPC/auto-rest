c93d267bfd6426858a99228701e0394f4d83b9c4
Daniel Perrefort <djperrefort@gmail.com>
2024-12-22 21:17:37 -0500

    Fix sqlite threading error

--- tests/compatibility_tests/test_sqlite.py
+++ tests/compatibility_tests/test_sqlite.py
@@ -1,5 +1,6 @@
 """Compatibility tests for SQLite databases."""
 
+import tempfile
 from unittest import TestCase
 
 from tests.compatibility_tests.base import AbstractCompatibilityTest
@@ -9,7 +10,21 @@ class SimpleTest(AbstractCompatibilityTest, TestCase):
     """Base compatibility tests applied to a SQLite database."""
 
     @classmethod
+    def setUpClass(cls) -> None:
+        """Create a temporary SQLite file."""
+
+        cls.temp_file = tempfile.NamedTemporaryFile(suffix='.sqlite')
+        super().setUpClass()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """Clean up temporary files."""
+
+        cls.temp_file.close()
+        super().tearDownClass()
+
+    @classmethod
     def get_db_url(cls) -> str:
         """Return the URL of the database to test compatibility against."""
 
-        return "sqlite:///:memory:"
+        return f"sqlite:///{cls.temp_file.name}"
