f9688ad586ac4724406888397834418089f61e65
Daniel Perrefort <djperrefort@gmail.com>
2024-12-26 18:05:55 -0500

    Adds support for async database drivers to endpoint handlers (#14)

--- tests/dependencies/test_apply_ordering_params.py
+++ tests/dependencies/test_apply_ordering_params.py
@@ -1,88 +1,63 @@
 from unittest import TestCase
+from unittest.mock import Mock
 
-from sqlalchemy import Column, create_engine, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from fastapi import Response
+from sqlalchemy.sql import select
+from sqlalchemy.sql.expression import asc, desc, Select
 
 from auto_rest.dependencies import apply_ordering_params
 
-Base = declarative_base()
-
-
-class MockModel(Base):
-    """A dummy database table to set up testing fixtures."""
-
-    __tablename__ = "mock_table"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-
 
 class TestApplyOrderingParams(TestCase):
-    """Unit tests for the apply_ordering_params function."""
+    """Unit tests for the `apply_ordering_params` function."""
 
     @classmethod
-    def setUpClass(cls):
-        """Set up a temporary in-memory database and session."""
+    def setUpClass(cls) -> None:
+        """Set up common test variables."""
 
-        cls.engine = create_engine("sqlite:///:memory:")
-        Base.metadata.create_all(cls.engine)
+        cls.response = Mock(spec=Response)
+        cls.query = select()
 
-        cls.Session = sessionmaker(bind=cls.engine)
-        cls.session = cls.Session()
-        cls.session.add_all([
-            MockModel(id=1, name="Bob"),
-            MockModel(id=2, name="Alice"),
-            MockModel(id=3, name="Charlie"),
-        ])
-        cls.session.commit()
+    def test_valid_params_ascending(self) -> None:
+        """Test parameters are applied correctly for ascending order."""
 
-    @classmethod
-    def tearDownClass(cls):
-        """Dispose of the database connection."""
+        params = {"order_by": "column_name", "direction": "asc"}
+        result_query = apply_ordering_params(self.query, params, self.response)
+        self.assertIsInstance(result_query, Select)
 
-        cls.session.close()
-        cls.engine.dispose()
+        order_clause = result_query._order_by_clauses[0]
+        self.assertEqual(str(order_clause), str(asc("column_name")))
 
-    def test_no_ordering_params(self) -> None:
-        """Test when no ordering parameters are provided."""
+    def test_valid_params_descending(self) -> None:
+        """Test parameters are applied correctly for descending order."""
 
-        query = self.session.query(MockModel)
-        ordered_query = apply_ordering_params(query, MockModel, {})
-        self.assertEqual(query.all(), ordered_query.all())
+        params = {"order_by": "column_name", "direction": "desc"}
+        result_query = apply_ordering_params(self.query, params, self.response)
+        self.assertIsInstance(result_query, Select)
 
-    def test_valid_ordering_asc(self) -> None:
-        """Test ordering by a valid column in ascending order."""
+        order_clause = result_query._order_by_clauses[0]
+        self.assertEqual(str(order_clause), str(desc("column_name")))
 
-        query = self.session.query(MockModel)
-        ordering_params = {"order_by": "name", "direction": "asc"}
+    def test_invalid_direction_param(self) -> None:
+        """Test an invalid direction defaults to ascending order."""
 
-        ordered_query = apply_ordering_params(query, MockModel, ordering_params)
-        self.assertEqual(["Alice", "Bob", "Charlie"], [row.name for row in ordered_query.all()])
+        params = {"order_by": "column_name", "direction": "invalid"}
+        result_query = apply_ordering_params(self.query, params, self.response)
+        self.assertIsInstance(result_query, Select)
 
-    def test_valid_ordering_desc(self) -> None:
-        """Test ordering by a valid column in descending order."""
+        order_clause = result_query._order_by_clauses[0]
+        self.assertEqual(str(order_clause), str(asc("column_name")))
 
-        query = self.session.query(MockModel)
-        ordering_params = {"order_by": "name", "direction": "desc"}
+    def test_no_direction_param(self) -> None:
+        """Test that ascending order is applied by default when 'direction' is missing."""
 
-        ordered_query = apply_ordering_params(query, MockModel, ordering_params)
-        self.assertEqual(["Charlie", "Bob", "Alice"], [row.name for row in ordered_query.all()])
+        params = {"order_by": "column_name"}
+        with self.assertRaises(ValueError):
+            apply_ordering_params(self.query, params, self.response)
 
-    def test_invalid_order_by_column(self):
-        """Test ordering is ignored for an invalid column name."""
+    def test_no_order_by_param(self) -> None:
+        """Test that no ordering is applied when 'order_by' is missing."""
 
-        query = self.session.query(MockModel)
-        ordering_params = {"order_by": "invalid_column", "direction": "asc"}
-
-        ordered_query = apply_ordering_params(query, MockModel, ordering_params)
-        self.assertEqual(query.all(), ordered_query.all())
-
-    def test_invalid_direction(self) -> None:
-        """Test ordering defaults to ascending for an invalid direction."""
-
-        query = self.session.query(MockModel)
-        ordering_params = {"order_by": "name", "direction": "invalid"}
-
-        ordered_query = apply_ordering_params(query, MockModel, ordering_params)
-        self.assertEqual(["Alice", "Bob", "Charlie"], [row.name for row in ordered_query.all()], )
+        params = {"direction": "asc"}
+        with self.assertRaises(ValueError):
+            apply_ordering_params(self.query, params, self.response)
