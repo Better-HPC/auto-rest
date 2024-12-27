481ac2893e412d4e2d22a559c86c2bf3613b77f5
Daniel Perrefort <djperrefort@gmail.com>
2024-12-27 00:04:52 -0500

    Adds tests for ordering headers.

--- tests/utils/test_apply_ordering_params.py
+++ tests/utils/test_apply_ordering_params.py
@@ -10,12 +10,11 @@ from auto_rest.utils import apply_ordering_params
 class TestApplyOrderingParams(TestCase):
     """Unit tests for the `apply_ordering_params` function."""
 
-    @classmethod
-    def setUpClass(cls) -> None:
+    def setUp(self) -> None:
         """Set up common test variables."""
 
-        cls.response = Response()
-        cls.query = select()
+        self.response = Response()
+        self.query = select()
 
     def test_valid_params_ascending(self) -> None:
         """Test parameters are applied correctly for ascending order."""
@@ -27,6 +26,10 @@ class TestApplyOrderingParams(TestCase):
         order_clause = result_query._order_by_clauses[0]
         self.assertEqual(str(asc("column_name")), str(order_clause))
 
+        self.assertEqual("true", self.response.headers.get("X-Order-Applied"))
+        self.assertEqual("column_name", self.response.headers.get("X-Order-By"))
+        self.assertEqual("asc", self.response.headers.get("X-Order-Direction"))
+
     def test_valid_params_descending(self) -> None:
         """Test parameters are applied correctly for descending order."""
 
@@ -37,11 +40,19 @@ class TestApplyOrderingParams(TestCase):
         order_clause = result_query._order_by_clauses[0]
         self.assertEqual(str(desc("column_name")), str(order_clause))
 
+        self.assertEqual("true", self.response.headers.get("X-Order-Applied"))
+        self.assertEqual("column_name", self.response.headers.get("X-Order-By"))
+        self.assertEqual("desc", self.response.headers.get("X-Order-Direction"))
+
     def test_missing_params(self) -> None:
         """Test ordering is not applied when parameters are not provided."""
 
         result_query = apply_ordering_params(self.query, {}, self.response)
         self.assertFalse(result_query._order_by_clauses)
+
+        self.assertEqual("false", self.response.headers.get("X-Order-Applied"))
+        self.assertEqual(None, self.response.headers.get("X-Order-By"))
+        self.assertEqual(None, self.response.headers.get("X-Order-Direction"))
 
     def test_missing_order_by_param(self):
         """Test ordering is not applied when the `order_by` parameter is not provided."""
@@ -49,6 +60,10 @@ class TestApplyOrderingParams(TestCase):
         params = {"direction": "desc"}
         result_query = apply_ordering_params(self.query, params, self.response)
         self.assertFalse(result_query._order_by_clauses)
+
+        self.assertEqual("false", self.response.headers.get("X-Order-Applied"))
+        self.assertEqual(None, self.response.headers.get("X-Order-By"))
+        self.assertEqual(None, self.response.headers.get("X-Order-Direction"))
 
     @skip("This test requires implementing additional testing structures.")
     def test_invalid_order_by_param(self):
@@ -64,6 +79,10 @@ class TestApplyOrderingParams(TestCase):
 
         order_clause = result_query._order_by_clauses[0]
         self.assertEqual(str(asc("column_name")), str(order_clause))
+
+        self.assertEqual("true", self.response.headers.get("X-Order-Applied"))
+        self.assertEqual("column_name", self.response.headers.get("X-Order-By"))
+        self.assertEqual("asc", self.response.headers.get("X-Order-Direction"))
 
     def test_invalid_direction_param(self) -> None:
         """Test a ValueError is raised for an invalid `direction` parameter."""
