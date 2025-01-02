51f87649bfed3179f13c13b699edc29f6efc7e80
Daniel Perrefort <djperrefort@gmail.com>
2025-01-02 18:08:00 -0500

    Simplifies tests for configure_logging

--- tests/app/test_configure_logging.py
+++ tests/app/test_configure_logging.py
@@ -7,47 +7,25 @@ from auto_rest.app import configure_logging
 class TestConfigureLogging(TestCase):
     """Unit tests for the `configure_logging` function."""
 
-    def test_log_level_debug(self) -> None:
-        """Test logging configuration for the `DEBUG` level."""
+    def test_log_level_is_set(self) -> None:
+        """Test the logging level is configured."""
 
-        configure_logging("DEBUG")
-        self.assertEqual(logging.DEBUG, logging.getLogger().level)
+        for log_level_str in ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"):
+            configure_logging(log_level_str)
 
-    def test_log_level_info(self) -> None:
-        """Test logging configuration for the `INFO` level."""
+            log_level_int = logging.getLevelNamesMapping()[log_level_str]
+            self.assertEqual(log_level_int, logging.getLogger().level)
 
-        configure_logging("INFO")
-        self.assertEqual(logging.INFO, logging.getLogger().level)
-
-    def test_log_level_warning(self) -> None:
-        """Test logging configuration for the `WARNING` level."""
-
-        configure_logging("WARNING")
-        self.assertEqual(logging.WARNING, logging.getLogger().level)
-
-    def test_log_level_error(self) -> None:
-        """Test logging configuration for the `ERROR` level."""
-
-        configure_logging("ERROR")
-        self.assertEqual(logging.ERROR, logging.getLogger().level)
-
-    def test_log_level_critical(self) -> None:
-        """Test logging configuration for the `CRITICAL` level."""
-
-        configure_logging("CRITICAL")
-        self.assertEqual(logging.CRITICAL, logging.getLogger().level)
-
-    def test_invalid_log_level(self) -> None:
-        """Test an invalid logging level raises an error."""
-
-        with self.assertRaisesRegex(ValueError, "Invalid logging level"):
-            # noinspection PyTypeChecker
-            configure_logging("INVALID")
-
-    def test_log_format(self):
+    def test_log_format_is_set(self) -> None:
         """Test the logging format is configured."""
 
         configure_logging("INFO")
         handler = logging.getLogger().handlers[0]
         self.assertIsInstance(handler, logging.StreamHandler)
         self.assertEqual(handler.formatter._fmt, "%(levelprefix)s %(message)s")
+
+    def test_invalid_log_level(self) -> None:
+        """Test an invalid logging level raises an error."""
+
+        with self.assertRaisesRegex(ValueError, "Invalid logging level"):
+            configure_logging("INVALID")
