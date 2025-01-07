7b2e6cb9dce323b244a3abec5963fedbea04f0f0
Daniel Perrefort <djperrefort@gmail.com>
2025-01-06 21:29:20 -0500

    Adds support for fields without defaults in dynamic interfaces

--- auto_rest/models.py
+++ auto_rest/models.py
@@ -5,10 +5,8 @@ import logging
 from pathlib import Path
 from typing import Callable
 
-import pydantic
-from pydantic.main import ModelT
+from pydantic.main import create_model, ModelT
 from sqlalchemy import create_engine, Engine, MetaData, URL
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, Session
 
@@ -85,24 +83,15 @@ def create_db_engine(url: URL, **kwargs) -> DBEngine:
 
     logger.info(f"Building database engine for {url}.")
 
-    # Attempt to create an async engine by default.
-    try:
+    if url.get_dialect().is_async:
         engine = create_async_engine(url, **kwargs)
         logger.debug("Asynchronous connection established.")
         return engine
 
-    except InvalidRequestError as e:
-        logger.warning(f"Async connection failed, falling back to sync. Error: {e}")
-
-    # Fall back to a synchronous engine if async fails.
-    try:
+    else:
         engine = create_engine(url, **kwargs)
         logger.debug("Synchronous connection established.")
         return engine
-
-    except Exception as e:  # pragma: no cover
-        logger.error(f"Could not connect to the database: {e}")
-        raise
 
 
 async def _async_reflect_metadata(engine: AsyncEngine, metadata: MetaData) -> None:
@@ -180,9 +169,12 @@ def create_db_interface(model: DBModel) -> type[ModelT]:
         A Pydantic model class with the same structure as the provided SQLAlchemy model.
     """
 
-    # Dynamic Pydantic models require a map of column names to their type and default value.
-    columns = {col.name: (col.type.python_type, col.default) for col in model.__table__.columns}
-    return pydantic.create_model(model.__name__, **columns)
+    fields = {
+        col.name: (col.type.python_type, col.default if col.default is not None else ...)
+        for col in model.__table__.columns
+    }
+
+    return create_model(model.__name__, **fields)
 
 
 def create_session_iterator(engine: DBEngine) -> Callable[[], DBSession]:
