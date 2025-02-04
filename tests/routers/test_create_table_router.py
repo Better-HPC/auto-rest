5e4d0932fe0d163b877b55dea35de56e17faf5a9
Daniel Perrefort <djperrefort@gmail.com>
2025-02-03 22:03:57 -0500

    Enables branch coverage and updates tests (#86)

--- tests/routers/test_create_table_router.py
+++ tests/routers/test_create_table_router.py
@@ -14,18 +14,23 @@ class TestCreateTableRouter(TestCase):
         """Create a mock SQLAlchemy engine."""
 
         cls.mock_engine = MagicMock()
-        metadata = MetaData()
+        cls.no_pk_table = Table(
+            "no_pk_model",
+            MetaData(),
+            Column("id", Integer),
+            Column("name", String),
+        )
 
         cls.single_pk_table = Table(
             "single_pk_model",
-            metadata,
+            MetaData(),
             Column("id", Integer, primary_key=True),
             Column("name", String),
         )
 
         cls.multiple_pk_table = Table(
             "multiple_pk_model",
-            metadata,
+            MetaData(),
             Column("id1", Integer, primary_key=True),
             Column("id2", Integer, primary_key=True),
             Column("name", String),
@@ -57,7 +62,7 @@ class TestCreateTableRouter(TestCase):
         self.assertCountEqual(expected_routes, routes)
 
     def test_multiple_primary_keys(self) -> None:
-        """Verify router paths include path parameters for tables with a multiple primary keys."""
+        """Verify router paths include path parameters for tables with multiple primary keys."""
 
         router = create_table_router(self.mock_engine, self.multiple_pk_table, writeable=True)
         actual_routes = [(route.path, method) for route in router.routes for method in route.methods]
@@ -68,6 +73,18 @@ class TestCreateTableRouter(TestCase):
             ("/{id1}/{id2}/", "PUT"),
             ("/{id1}/{id2}/", "PATCH"),
             ("/{id1}/{id2}/", "DELETE"),
+        ]
+
+        self.assertCountEqual(expected_routes, actual_routes)
+
+    def test_no_primary_keys(self) -> None:
+        """Verify router paths do not include PK endpoints for tables with no primary keys."""
+
+        router = create_table_router(self.mock_engine, self.no_pk_table, writeable=True)
+        actual_routes = [(route.path, method) for route in router.routes for method in route.methods]
+        expected_routes = [
+            ("/", "GET"),
+            ("/", "POST"),
         ]
 
         self.assertCountEqual(expected_routes, actual_routes)
